ec953b05a5532f97e62600430bbe93c4e4b0256e
Angela Chen <pic477@ensign.edu>
2023-04-07 00:15:08 -0600

    feat: Finally Done

--- header.js
+++
@@ -1,8 +0,0 @@
-function downloadFile(filePath) {
-    const a = document.createElement('a');
-    a.href = filePath;
-    a.download = 'Angela Chen resume';
-    a.click();
-}
-
-window.downloadFile = downloadFile;
---
+++ src/helpers/experience.js
@@ -0,0 +1,36 @@
+function experience() {
+  return {
+    jobs: [
+      {
+        title: "Homeschool Teacher",
+        date: "Mar 2020-Mar 2022",
+        description:
+          "  ●\tGuided more than 15 students aged 6 to 14 through practice activities in \n" +
+          "              Math, English, Mandarin, Science, and Social Studies.\n" +
+          "              ●\tMentored more than 10 children aged 2 to 5 with self-care ability, training them to have lunch and dinner by themselves,\n" +
+          "                 educating emotional intelligence, and coaching them to clean their bedroom and toys.\n",
+        duties: "Write performant and maintainable code with Vue.Js.",
+      },
+      {
+        title: "Wedding Planner",
+        date: "Jan 2019-Jan 2020",
+        description:
+          "        I have had some pretty cool experiences! I've developed two mobile apps using UIKit and SwiftUI, which\n" +
+          "                are great tools for\n" +
+          "                creating user interfaces on Apple platforms. I've also explored cross-platform development by creating a\n" +
+          "                proof of concept\n" +
+          "                Flutter app. I even got to convert C++ XML to TypeScript JSON for a REST API, which was challenging but\n" +
+          "                rewarding work. On top\n" +
+          "                of all that, I have maintained an Android app using Kotlin, Java, and C++, which has helped me to gain a\n" +
+          "                solid understanding\n" +
+          "                of mobile app development. And lastly, I have experience with continuous integration and continuous\n" +
+          "                delivery (CI/CD)\n" +
+          "                processes, which are important tools for ensuring that software is delivered efficiently and with high\n" +
+          "                quality.",
+        duties: "Write performant and maintainable code with Vue.Js.",
+      },
+    ],
+  };
+}
+
+window.experience = experience;
